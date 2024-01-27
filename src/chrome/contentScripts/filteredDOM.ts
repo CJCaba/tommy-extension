3b75321a9fda49833796aae5bf773504ccd2ad59
jadec <cabansa3@msu.edu>
2024-01-27 18:45:24 -0500

    Initial Class Object and Association Constructor

--- src/chrome/contentScripts/filteredDOM.ts
+++ src/chrome/contentScripts/filteredDOM.ts
@@ -9,36 +9,39 @@ const SEARCH_NODES = [];
 
 // Class for our data structure holding the important data from nodes
 class Tommy_Node {
-    tag: Element;
+    tag: HTMLElement;
     data: string;
     assoc: Tommy_Node[];
     key: string;
-    constructor(tag: Element, data: string, key: string) {
+    parent?: Tommy_Node;
+    constructor(tag: HTMLElement, data: string, key: string, parent?: Tommy_Node) {
         this.tag = tag;
         this.data = data;
         this.assoc = [];
         this.key = key;
+        this.parent = parent;
     }
-
-    
+   
 }
-
 
 export function FilteredDOM() {
 
-    // Get the root node of the DOM
-    const node = document.documentElement;
+    // Get the body node of the DOM
+    const node = document.getElementsByTagName("body");
 
-    function traverseDOM(node: Node): void{
+    const root = new Tommy_Node(node[0], node[0].textContent as string, "random", undefined);
+
+    function traverseDOM(node: Node, parent: Tommy_Node): void{
         // Node is the current DOM node of the file
     
         // First check to determine whether the node is a proper element node
         if (node.nodeType === Node.ELEMENT_NODE) {
-            const element = node as Element;
-
+            const element = node as HTMLElement;
+        
             // Then check to see if the nodeName is one of the important tags to mark down
             if (TAGS.includes(element.nodeName)) {
-                const new_node = new Tommy_Node(element, element.textContent as string, "random_string");
+                const new_node = new Tommy_Node(element, element.textContent as string, "random_string", parent);
+                parent = new_node;
                 element.setAttribute(TV, "random_string");
                 SEARCH_NODES.push(new_node);
             }
@@ -46,10 +49,10 @@ export function FilteredDOM() {
 
         // Visit each child of the current node
         node.childNodes.forEach(childNode => {
-            traverseDOM(childNode);
+            traverseDOM(childNode, parent);
         });
     }
 
-    traverseDOM(node);
+    traverseDOM(node[0], root);
 }
 
